2935fc965b52bda7af90b60ae31906fbb61c8804
Uzayisenga <aminauzayisenga@gmail.com>
2020-04-03 17:39:47 +0200

    first commit

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,7 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link jquery.js
+//= link bootstrap.min.js
+//= link owl.carousel.min.js
+//= link smoothscroll.js
+//= link custom.js
